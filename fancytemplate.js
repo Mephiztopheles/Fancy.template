8741505b4250d09fdc1243f65a311f08eadde039
Markus Ahrweiler <markus.ahrweiler@web.de>
2015-12-02 21:58:24 +0100

    Version 0.0.4:  - Filter per instance

--- fancytemplate.js
+++ fancytemplate.js
@@ -1,149 +1,3 @@
-(function( window ) {
-    var IDENTIFIER = /^[a-zA-Z][a-zA-Z0-9]*/;
-    var NUMBER     = /^-?[0-9]+(\.[0-9]+)?/;
-    var COMMENT    = /^\/\/.*/;
-    var WHITESPACE = /^[^\n\S]+/;
-    var INDENT     = /^(?:\n[^\n\S]*)+/;
-    var OPTABLE    = {
-        '+' : 'PLUS',
-        '-' : 'MINUS',
-        '*' : 'MULTIPLY',
-        '.' : 'DOT',
-        '\\': 'BACKSLASH',
-        ':' : 'COLON',
-        '%' : 'PERCENT',
-        '|' : 'PIPE',
-        '!' : 'EXCLAMATION',
-        '?' : 'QUESTION',
-        '#' : 'POUND',
-        '&' : 'AMPERSAND',
-        ';' : 'SEMI',
-        ',' : 'COMMA',
-        '(' : 'L_PARENTHESIS',
-        ')' : 'R_PARENTHESIS',
-        '<' : 'L_ANG',
-        '>' : 'R_ANG',
-        '{' : 'L_BRACE',
-        '}' : 'R_BRACE',
-        '[' : 'L_BRACKET',
-        ']' : 'R_BRACKET',
-        '=' : 'EQUALS'
-    };
-
-
-    function Lexer( expression ) {
-        if( !(this instanceof Lexer) ) {
-            return new Lexer( expression );
-        }
-        this.tokens = [];
-        this.indent = 0;
-        this.chunk  = undefined;
-        return this.tokenise( expression );
-    }
-
-    Lexer.api = Lexer.prototype = {};
-    Lexer.api.identifier = function() {
-        var value,
-            token = IDENTIFIER.exec( this.chunk );
-        if( token ) {
-            value = token[ 0 ];
-            this.tokens.push( [ "IDENTIFIER", value ] );
-            return value.length;
-        }
-
-        return 0;
-    };
-    Lexer.api.number     = function() {
-        var token = NUMBER.exec( this.chunk );
-        if( token ) {
-            this.tokens.push( [ 'NUMBER', token[ 0 ] ] );
-            return token[ 0 ].length;
-        }
-
-        return 0;
-    };
-    Lexer.api.string     = function() {
-        var firstChar = this.chunk.charAt( 0 ),
-            quoted    = false,
-            nextChar;
-        if( firstChar == '"' || firstChar == "'" ) {
-            for( var i = 1; i < this.chunk.length; i++ ) {
-                if( !quoted ) {
-                    nextChar = this.chunk.charAt( i );
-                    if( nextChar == "\\" ) {
-                        quoted = true;
-                    } else if( nextChar == firstChar ) {
-                        this.tokens.push( [ 'STRING', this.chunk.substring( 0, i + 1 ) ] );
-                        return i + 1;
-                    }
-                } else {
-                    quoted = false;
-                }
-            }
-        }
-
-        return 0;
-    };
-    Lexer.api.comment    = function() {
-        var token = COMMENT.exec( this.chunk );
-        if( token ) {
-            this.tokens.push( [ 'COMMENT', token[ 0 ] ] );
-            return token[ 0 ].length;
-        }
-
-        return 0;
-    };
-    Lexer.api.whitespace = function() {
-        var token = WHITESPACE.exec( this.chunk );
-        if( token ) {
-            return token[ 0 ].length;
-        }
-
-        return 0;
-    };
-    Lexer.api.line       = function() {
-        var token = INDENT.exec( this.chunk );
-        if( token ) {
-            var lastNewline = token[ 0 ].lastIndexOf( "\n" ) + 1;
-            var size        = token[ 0 ].length - lastNewline;
-            if( size > this.indent ) {
-                this.tokens.push( [ 'INDENT', size - this.indent ] );
-            } else {
-                if( size < this.indent ) {
-                    this.tokens.push( [ 'OUTDENT', this.indent - size ] );
-                }
-                this.tokens.push( [ 'TERMINATOR', token[ 0 ].substring( 0, lastNewline ) ] );
-            }
-            this.indent = size;
-            return token[ 0 ].length;
-        }
-
-        return 0;
-    };
-    Lexer.api.literal    = function() {
-        var tag = this.chunk.slice( 0, 1 );
-        if( OPTABLE[ tag ] ) {
-            this.tokens.push( [ OPTABLE[ tag ], tag ] );
-            return 1;
-        }
-
-        return 0;
-    };
-    Lexer.api.tokenise   = function( source ) {
-        var i = 0;
-        while( this.chunk = source.slice( i ) ) {
-            var diff = this.identifier() || this.number() || this.string() || this.comment() || this.whitespace() || this.line() || this.literal();
-            if( !diff ) {
-                console.error( "Couldn't tokenise: " + this.chunk + " near \"" + source.slice( Math.max( 0, i - 15 ), i + 15 ) + "\"" );
-                return;
-            }
-            i += diff;
-        }
-
-        return this.tokens;
-    };
-    window.Lexer         = Lexer;
-})( window );
 (function( $ ) {
     Fancy.require( {
         jQuery: false,
@@ -151,9 +5,8 @@
     } );
     var id            = 0,
         NAME          = "FancyTemplate",
-        VERSION       = "0.0.3",
+        VERSION       = "0.0.4",
         templateCache = {},
-        FILTER        = {},
         SINGLETAGS    = [ "br", "link", "img", "meta", "param", "input", "source", "track" ],
         STRIPTAGS     = [ "b", "i", "u" ],
         NODETYPE      = {
@@ -206,7 +59,7 @@
 
     function $eval( scope, expression ) {
 
-        var lexer = new Lexer( expression );
+        var lexer = new Fancy.Lexer( expression );
         lexer.forEach( function( it, i ) {
             if( parse( it, i, lexer ) ) {
                 throw new FancyTemplateError( "Eval", "Syntax Error: Token '" + it[ 1 ] + "' is an unexpected token at " + expression );
@@ -249,6 +102,7 @@
         this.settings = $.extend( {}, Fancy.settings [ NAME ], settings );
         this.id       = id++;
         this.parsed   = [];
+        this.$filter  = {};
         if( !logged ) {
             logged = true;
             Fancy.version( SELF );
@@ -279,13 +133,13 @@
         this.parsed.forEach( function( it ) {
             var expressions = getExpression( l, r );
             it.parsed       = it.expression.replace( expressions, function( match, $1 ) {
-                return FancyTemplate.eval( SELF.settings.scope, $1 );
+                return SELF.eval( $1 );
             } );
         } );
         this.parsed.forEach( function( it, i ) {
             it.node.nodeValue = it.parsed;
             if( it.nodeType === NODETYPE.comment && it.node.nodeType === it.nodeType ) {
-                var newNode           = $( document.createTextNode( it.parsed ) );
+                var newNode = $( document.createTextNode( it.parsed ) );
                 $( it.node ).replaceWith( newNode );
                 SELF.parsed[ i ].node = newNode[ 0 ];
             }
@@ -293,11 +147,28 @@
         return this;
     };
 
-    FancyTemplate.eval = function( scope, expression ) {
-        var evaluated = null;
+    FancyTemplate.api.eval = function( expression ) {
+        var evaluated = null,
+            SELF      = this;
         // only properties
         if( expression.match( new RegExp( "\\S+\\|(\\w)*" ) ) ) {
-            evaluated = FancyTemplate.filter( scope, expression.match( /\|(\w*)/ )[ 1 ], $eval( scope, expression.split( "|" )[ 0 ] ), expression.split( "|" )[ 1 ] );
+            evaluated = (function( scope, name, value, filter ) {
+                if( !SELF.$filter[ name ] ) {
+                    console.error( "You didn't define " + (name || "the filter") );
+                    return value;
+                }
+                var args    = [ value ];
+                var filters = filter.replace( name, "" ).split( ":" );
+                filters.splice( 0, 1 );
+                try {
+                    filters.forEach( function( it ) {
+                        args.push( $eval( scope, it ) );
+                    } );
+                    return SELF.$filter[ name ].apply( this, args );
+                } catch( e ) {
+                    return value;
+                }
+            })( scope, expression.match( /\|(\w*)/ )[ 1 ], $eval( scope, expression.split( "|" )[ 0 ] ), expression.split( "|" )[ 1 ] );
         } else {
             evaluated = $eval( scope, expression );
         }
@@ -305,24 +176,6 @@
             return "";
         }
         return evaluated;
-    };
-
-    FancyTemplate.filter = function( scope, name, value, filter ) {
-        if( !FILTER[ name ] ) {
-            console.error( "You didn't define " + (name || "the filter") );
-            return value;
-        }
-        var args    = [ value ];
-        var filters = filter.replace( name, "" ).split( ":" );
-        filters.splice( 0, 1 );
-        try {
-            filters.forEach( function( it ) {
-                args.push( $eval( scope, it ) );
-            } );
-            return FILTER[ name ].apply( this, args );
-        } catch( e ) {
-            return value;
-        }
     };
 
     FancyTemplate.api.compile = function() {
@@ -403,6 +256,13 @@
         this.element.removeData( NAME );
         return null;
     };
+    FancyTemplate.api.filter  = function( name, filter ) {
+        if( Fancy.getType( filter ) === "function" ) {
+            this.$filter[ name ] = filter;
+        } else {
+            console.error( "You can define " + (name || "a filter") + " only as function!" );
+        }
+    };
 
     Fancy.settings [ NAME ] = {
         scope         : {},
@@ -411,13 +271,6 @@
         bindClass     : NAME + "-bindings"
     };
 
-    Fancy.templateFilter     = function( name, filter ) {
-        if( Fancy.getType( filter ) === "function" ) {
-            FILTER[ name ] = filter;
-        } else {
-            console.error( "You can define " + (name || "a filter") + " only as function!" );
-        }
-    };
     Fancy.forbidTemplateTags = function() {
         $A( arguments ).forEach( function( it ) {
             var index       = STRIPTAGS.indexOf( it ),
